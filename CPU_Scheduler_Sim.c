9752fc4f2b9e79d8387824b89d52df2aeda33c46
noelw <noel.e.willems@gmail.com>
2020-10-27 22:54:50 -0400

    WIP update - MOSTLY adds new processes to process "queue." may need tweaks, like adjusting pointers and stuff.

--- CPU_Scheduler_Sim.c
+++ CPU_Scheduler_Sim.c
@@ -7,7 +7,8 @@
     a. Ignore comments (line starts with "//")
     b. Every line is: <process id> <cpu burst> <io burst> <repetitions> <priority>
     c. Example: 1 100 0 1 1 = process 1 spends 100 ticks in the cpu, 0 in io, occurs only once, and has a priority of 1 (low priority = first)
-2.  Based on the algorithm name, send to a specific method.
+2.  Make a struct for each process. Have the id, cpu time, io time, rep, and priority all be properties of the struct.
+3.  Add processes to the array of waiting processes.
 */
 
 #include <stdio.h>
@@ -15,19 +16,39 @@
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
+
+int idle_CPU_time;
+
+
+typedef struct Process {
+    int id; // id of process
+    int cpu_time; // time needed for CPU
+    int io_time; // time needed for I/O
+    int reps; // repetitions of process
+    int priority; // priority 
+    int start_time; // when the process begins
+    int end_time; // when the process ends
+    int wait_time; // how long process spends in RTR queue
+} Process;
+
+Process* rtr; // ready to run "queue"
 
 int main(int argc, char *argv[]) {
-    // Getting args from command line
+    // Malloc array of processes in rtr
+    rtr = (Process*)malloc(sizeof(Process) * 256);
+    // Make sure the user inputs the correct number of args. If not, make suggestion & exit.
     if(argc != 3) {
         printf("Please enter args like this: <program name> <process file name> <algorithm name>\n");
         exit(0);
     }
 
-    char* procFileName = argv[1]; // process file name
-    char* algorithmName = argv[2]; // algorithm name
+    char* proc_file_name = argv[1]; // process file name
+    char* algorithm_name = argv[2]; // algorithm name
+    char* allTokens[10];
 
-    // Processing input file
-    FILE * inputFile = fopen(procFileName, "r"); // file of list of processes
+    // Processing input file - if error, output message and return.
+    FILE * inputFile = fopen(proc_file_name, "r"); // file of list of processes
     if (inputFile == NULL) {
 		perror("Error opening file");
 		return (-1);
@@ -49,11 +70,40 @@ int main(int argc, char *argv[]) {
             *pos = '\0';
         }
 
-        // For comments and null lines
+        // Ignore comments and null lines.
         if((line[0] == '/') || line[0] == '\0') {
 
+        // Creating the processes, assigning their values, and adding them to the "queue"
         } else {
-            printf("line: %s\n", line);
+            // Malloc individual process
+            Process *proc = (Process*)malloc(sizeof(Process));
+            // Send to method that processes line char by char
+            char* token = strtok(line, " ");
+            proc->id = atoi(token);
+            printf("%s is the id\n", token);
+            int track = 1;
+            int procCount = 0;
+            while(token != NULL) {
+                if(track == 1) {
+                    proc->cpu_time = atoi(token);
+                    printf("%s is the cpu time\n", token);
+                }
+                if(track == 2) {
+                    proc->io_time = atoi(token);
+                    printf("%s is the io time\n", token);
+                }
+                if(track == 3) {
+                    proc->reps = atoi(token);
+                    printf("%s is the rep\n", token);
+                }
+                if(track == 4) {
+                    proc->priority = atoi(token);
+                    printf("%s is the pri\n", token);
+                }
+                track++;              
+                token = strtok(NULL, " ");
+            }
+            rtr[procCount] = proc;
         }
     }
 }
